bcea636b92e08f8ce64e03793aa42bb06f391f61
Jairo Blatt <jairoblattc@gmail.com>
2021-10-22 17:14:29 -0300

    chore: husky install (#3)

--- cypress/e2e/Home.spec.ts
+++ cypress/e2e/Home.spec.ts
@@ -18,8 +18,58 @@ context("Home", () => {
     cy.get(".table-item").should("have.length", 100);
   });
 
+  it("Should filter by gender", () => {
+    cy.get('[data-testid="filter-gender"]').click();
+
+    cy.get(".filter__options li")
+      .eq(2)
+      .click({ force: true });
+
+    cy.wait(1000);
+
+    cy.get("tbody tr td")
+      .eq(1)
+      .should("have.text", "male");
+  });
+
   it("Should filter by nationality", () => {
     cy.get('[data-testid="filter-nationality"]').click();
-    cy.get(".filter__options");
+
+    cy.get("[data-testid='filter-nationality'] .filter__options li")
+      .eq(2)
+      .click({ force: true });
+
+    cy.wait(1000);
+
+    cy.get("tbody tr td")
+      .last()
+      .click();
+
+    cy.get(".modal  .content__andress h2 span").should("have.text", "(Brazil)");
+  });
+
+  context("User", () => {
+    beforeEach(() => {
+      cy.visit("/user/silverzebra371?page=2&seed=test");
+    });
+
+    it("Should open modal with user", () => {
+      cy.get(".modal").should("be.visible");
+    });
+
+    it("Should close the modal user", () => {
+      cy.get('[data-testid="modal-close"]').click();
+      cy.get(".modal").should("not.be.visible");
+    });
+
+    it("Should copy link when user click in btn", () => {
+      cy.get("[data-testid='clipboard']").click();
+
+      cy.window().then((win) => {
+        win.navigator.clipboard.readText().then((text) => {
+          expect(text).toBe("http://localhost:8080/user/silverzebra371?page=2&seed=test");
+        });
+      });
+    });
   });
 });
